f1c31d988bb28f07b22a7a0478b2f96aa04907a6
Friedemann Stoffregen <friedemann.stoffregen@student.fh-kiel.de>
2018-01-24 09:50:11 +0100

    build improved

--- pru/overlay/build.sh
+++ pru/overlay/build.sh
@@ -3,3 +3,7 @@
 echo "Compiling the overlay from .dts to .dtbo"
 
 dtc -O dtb -o SAMPLER-GPIO-00A0.dtbo -b 0 -@ SAMPLER-GPIO.dts
+
+cp SAMPLER-GPIO.dts /lib/firmware
+cd /usr/src/bb.org-overlays/
+./install.sh
