fd511947aa8650ca74237b070fcbd801603f0fac
Friedemann Stoffregen <friedemann.stoffregen@student.fh-kiel.de>
2018-01-24 15:12:45 +0100

    test

--- pru/src/pru0adc.c
+++ pru/src/pru0adc.c
@@ -17,7 +17,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "resource_table_1.h"
+#include "resource_table_0.h"
 #include <am335x/pru_cfg.h>
 #include <am335x/pru_intc.h>
 #include <pru_rpmsg.h>
