e4969b91f315a19392a40da47ec603ce3990f348
Friedemann Stoffregen <friedemann.stoffregen@student.fh-kiel.de>
2018-01-30 11:09:54 +0100

    test

--- src/ADCManager.cpp
+++ src/ADCManager.cpp
@@ -24,7 +24,8 @@ struct ADCOut {
 
 void publish(ADCOut out, int queueValue) {
     MessageQueue queue = MessageQueue(queueValue);
-    std::cout << out.adc0 << "; " << out.adc1 << "; " << out.tone_adc02 << "; " << out.tone_adc03 << "; " << out.adc4 << "; " << out.adc5 << "; " << out.gpio6 << "; " << out.gpio7 <<  std::endl;
+    std::cout << out.adc0 << "; " << out.adc1 << "; " << out.tone_adc02 << "; " << out.tone_adc03 << "; " << out.adc4
+              << "; " << out.adc5 << "; " << out.gpio6 << "; " << out.gpio7 << std::endl;
     Event event = Event(EventType::ADC_VALUES);
     event.addString("ADC");
     event.addInt(0);
@@ -93,9 +94,11 @@ int main(int argc, char **argv) {
         if (readpru > 0) {
             ADCOut out;
             for (int j = 0; j < 7; j++) {
-                if ((buffer[j] & 0x8000) ) { // first bit is "1", so it's an GPIO value
+                if ((buffer[j] & 0x8000)) { // first bit is "1", so it's an GPIO value
                     // GPIO Input value
                     int16_t value = buffer[j];
+                    std::cout << "BUFFER: " << buffer[j] << "; GPIO6: " << (value >> 6) << "; GPIO7: " << (value >> 7)
+                              << std::endl;
                     out.gpio6 = static_cast<bool>((value >> 6) & 1);
                     out.gpio7 = static_cast<bool>((value >> 7) & 1);
                 } else {
